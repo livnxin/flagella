578dd170de849f7f37e2699004154a4779593a66
livnxin <lilixinxin@protonmail.com>
2026-09-08 17:24:07 +0700

    feat: replace machine learning with multi layer perceptron
    Signed-off-by: livnxin <lilixinxin@protonmail.com>

--- model/src/model/__init__.py
+++ model/src/model/__init__.py
@@ -1,8 +1,7 @@
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-from sklearn.linear_model import LinearRegression
-from sklearn.pipeline import Pipeline
+from keras import Sequential
 
 
 def main() -> None:
@@ -12,11 +11,18 @@ def main() -> None:
 
 def train() :
     housing = fetch_california_housing()
-    X_train, X_test, y_train, y_test = train_test_split(housing.data, housing.target)
-    quantile_transformer = preprocessing.QuantileTransformer(random_state=0)
-    pipe = Pipeline(steps=[
-        ('transform', preprocessing.QuantileTransformer(random_state=0)),
-        ('regression', LinearRegression())])
-    pipe.fit(X_train, y_train)
-    score = pipe.score(X_test, y_test)
-    print("THe score is ", score)
+    Xtrain, Xtest, ytrain, ytest = train_test_split(housing.data, housing.target, test_size=0.1)
+    Xtrain, Xval, ytrain, yval = train_test_split(Xtrain, ytrain, test_size=.2)
+    transformer = preprocessing.QuantileTransformer(random_state=0)
+    Xtrain = transformer.fit_transform(Xtrain)
+    Xtest = transformer.transform(Xtest)
+    Xval = transformer.transform(Xval)
+    model = Sequential([
+        Dense(30, activation = 'relu', input_shape= Xtrain.shape[1:]),
+        Dense(20, activation = 'relu'),
+        Dense(1)
+    ])
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(Xtrain, ytrain, validation_data=(Xval, yval), epochs=20)
+    mse_test = model.evaluate(Xtest, ytest)
+    print("The mean square error is ", mse)
