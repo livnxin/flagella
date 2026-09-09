ce096429ee9f9abe099cfd948e09af7bb100fd34
livnxin <lilixinxin@protonmail.com>
2026-09-09 09:39:44 +0700

    fix: modularize train() function by splitting it to prepare() and train()
    Signed-off-by: livnxin <lilixinxin@protonmail.com>

--- model/src/model/__init__.py
+++ model/src/model/__init__.py
@@ -10,19 +10,29 @@ def main() -> None:
     train()
     print("Training is over")
 
-def train() :
+
+def prepare():
     housing = fetch_california_housing()
-    Xtrain, Xtest, ytrain, ytest = train_test_split(housing.data, housing.target, test_size=0.1)
-    Xtrain, Xval, ytrain, yval = train_test_split(Xtrain, ytrain, test_size=.2)
+    Xtrain, Xtest, ytrain, ytest = train_test_split(
+        housing.data, housing.target, test_size=0.1
+    )
+    Xtrain, Xval, ytrain, yval = train_test_split(Xtrain, ytrain, test_size=0.2)
     transformer = preprocessing.QuantileTransformer(random_state=0)
     Xtrain = transformer.fit_transform(Xtrain)
     Xtest = transformer.transform(Xtest)
     Xval = transformer.transform(Xval)
-    model = Sequential([
-        layers.Dense(30, activation = 'relu', input_shape= Xtrain.shape[1:]),
-        layers.Dense(20, activation = 'relu'),
-        layers.Dense(1)
-    ])
+    return (Xtrain, Xtest, Xval, ytrain, ytest, yval)
+
+
+def train():
+    Xtrain, Xtest, Xval, ytrain, ytest, yval = prepare()
+    model = Sequential(
+        [
+            layers.Dense(30, activation="relu", input_shape=Xtrain.shape[1:]),
+            layers.Dense(20, activation="relu"),
+            layers.Dense(1),
+        ]
+    )
     model.compile(loss=loss, optimizer=optimizer)
     model.fit(Xtrain, ytrain, validation_data=(Xval, yval), epochs=epochs)
     mse_test = model.evaluate(Xtest, ytest)
