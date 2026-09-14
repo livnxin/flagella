80d371d56ff2f6ead6f9f3ece6236fa7330ebfd2
livnxin <lilixinxin@protonmail.com>
2026-09-14 13:24:54 +0700

    fix: refactor prediction service and fix tracing bug
    Signed-off-by: livnxin <lilixinxin@protonmail.com>

--- service/prediction/main.py
+++ service/prediction/main.py
@@ -4,13 +4,7 @@ from keras.saving import load_model
 from utils import predict
 from prometheus_fastapi_instrumentator import Instrumentator
 from prometheus_client import make_asgi_app
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    BatchSpanProcessor,
-    ConsoleSpanExporter,
-)
-from opentelemetry.semconv.trace import SpanAttributes
+from tracing import tracer
 
 print("Initializing prediction service")
 print("Loading model")
@@ -22,16 +16,6 @@ Instrumentator().instrument(app).expose(app)
 
 metrics_app = make_asgi_app()
 app.mount("/metrics", metrics_app)
-
-provider = TracerProvider()
-processor = BatchSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(processor)
-
-# Sets the global default tracer provider
-trace.set_tracer_provider(provider)
-
-# Creates a tracer from the global tracer provider
-tracer = trace.get_tracer("my.tracer.name")
 
 
 class Input(BaseModel):
--- service/prediction/utils.py
+++ service/prediction/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
-import main
+
+from tracing import tracer
 
 FEATURE_ORDER = [
     "MedInc",
